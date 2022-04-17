9e0cba974e00dccdef9cd1200131a8a3e367c5ac
Maksym Shokin <problaze7@gmail.com>
2022-04-17 16:53:41 +0300

    basic-resolvers-and-schema

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('works1')
+import { ApolloServer, gql } from 'apollo-server';
+import { Query } from './resolvers';
+import { typeDefs } from './schema';
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers: {
+    Query
+  }
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
